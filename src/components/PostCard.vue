ba9816e4c14687f008914f83e56a977d9dbcb401
Ryan Wang <i@ryanc.cc>
2022-07-29 20:43:20 +0800

    chore: add eslint prettier
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -15,13 +15,13 @@ function resetIsShowing() {
 
 <template>
   <div class="p-4">
-    <span class="font-bold text-xl" th:text="@{post.title}">Hello Halo</span>
+    <span class="text-xl font-bold" th:text="@{post.title}">Hello Halo</span>
 
     <div class="flex flex-col items-center py-16">
       <div class="h-32 w-32">
         <TransitionRoot
-          appear
           :show="isShowing"
+          appear
           as="template"
           enter="transform transition duration-[400ms]"
           enter-from="opacity-0 rotate-[-120deg] scale-50"
@@ -34,10 +34,10 @@ function resetIsShowing() {
         </TransitionRoot>
       </div>
       <button
-        @click="resetIsShowing"
         class="mt-8 flex transform items-center rounded-full bg-black bg-opacity-20 px-3 py-2 text-sm font-medium text-white transition hover:scale-105 hover:bg-opacity-30 focus:outline-none active:bg-opacity-40"
+        @click="resetIsShowing"
       >
-        <svg viewBox="0 0 20 20" fill="none" class="h-5 w-5 opacity-70">
+        <svg class="h-5 w-5 opacity-70" fill="none" viewBox="0 0 20 20">
           <path
             d="M14.9497 14.9498C12.2161 17.6835 7.78392 17.6835 5.05025 14.9498C2.31658 12.2162 2.31658 7.784 5.05025 5.05033C7.78392 2.31666 12.2161 2.31666 14.9497 5.05033C15.5333 5.63385 15.9922 6.29475 16.3266 7M16.9497 2L17 7H16.3266M12 7L16.3266 7"
             stroke="currentColor"
